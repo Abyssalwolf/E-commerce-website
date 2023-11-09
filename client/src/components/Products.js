d5a621942847df63590eb04574d99e5d15ec9858
Midhunnnnnnnn <midhunsmanoj04@gmail.com>
2023-11-10 00:31:51 +0530

    product update

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React from "react";
 
-const Products= () => {
-    return
-    <div className="py-10">
-        <div class="w-full relative group">
+const Products = () => {
+  return;
+  <div className="py-10">
+    <div class="py-10">
+  <div class="flex flex-col items-center gap-4">
+    <h1 class="text-2xl bg-black text-white py-2 w-80 text-center">shopping everyday</h1>
+    <span class="w-20 h-[3px] bg-black"></span>
+    <p class="max-w-[700px] text-gray-600 text-center">
+      WELCOME TO URBAN HUB :WHERE YOUR CHOICES MEET YOUR DESIRES
+    </p>
+  </div>
+  
+  <div class="max-w-screen-xl mx-auto grid grid-cols-4 gap-10 py-10">
+    <div class="w-full relative group">
+      <div class="w-full h-96 cursor-pointer overflow-hidden">
+        <img class="w-full h-full object-cover group-hover:scale-110 duration-500" src="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=600" alt="productImg"></img>
+      </div>
+      <div class="w-full border-[1px] px-2 py-4">
+        <div class="flex justify-between items-center">
+          <div>
+            <h2 class="font-titleFont text-base font-bold">Long sleeve Jac</h2>
+          </div>
+          <div class="text-sm relative w-28 flex justify-end overflow-hidden">
+            <div class="flex gap-2 transform group-hover:translate-x-24 transition-transform duration-500">
+              <p class="line-through text-gray-500">$200</p>
+              <p class="font-semibold">$150</p>
+            </div>
+            <p class="absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500">
+              add to cart
+              <span>
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                </svg>
+              </span>
+            </p>
+          </div>
+        </div>
+        <div>
+          <p>women</p>
+        </div>
+      </div>
+      <div class="absolute top-4 right-0">
+        <p class="bg-black text-white font-semibold font-titleFont px-6 py-1">Sale</p>
+      </div>
+      <div class="Toastify"></div>
+    </div>
+   
+  </div>
+</div>
+<div class="w-full relative group">
     <div class="w-full h-96 cursor-pointer overflow-hidden">
         <img class="w-full h-full object-cover group-hover:scale-110 duration-500" src="https://images.pexels.com/photos/2752045/pexels-photo-2752045.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=600" alt="productImg"></img>
-    </div>
+ </div>
     <div class="w-full border-[1px] px-2 py-4">
         <div class="flex justify-between items-center">
             <div>
@@ -52,12 +98,26 @@ const Products= () => {
                 </div>
                 <p class="absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500">
                     add to cart
-                    </p>
-</div>
-</div>
-</div>
-</div>
-</div>
+                    <span>
+                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                        </svg>
+                    </span>
+                </p>
+            </div>
+            <div>
+                <p>women</p>
+            </div>
+        </div>
+        <div class="absolute top-4 right-0">
+            <p class="bg-black text-white font-semibold font-titleFont px-6 py-1">Sale</p>
+        </div>
+        <div class="Toastify"></div>
+    </div>
+    
 
+                    
+  </div>
+  </div>
 }
 export default Products;
