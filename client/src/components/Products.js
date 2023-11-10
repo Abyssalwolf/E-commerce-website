5cc0ef84a8e8e719e96996c2db9d4150a3c9805f
NANDANA S <nandana5434@gmail.com>
2023-11-10 23:05:23 +0530

    added images to products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -53,81 +53,81 @@ const Products = () => {
       </div>
       <div className="max-w-screen-xl mx-auto grid grid-cols-4 gap-10 py-10">
         <Product
-          title="Long sleeve Jac"
+          title="Burberry"
          
           newPrice="$150"
           category="Women"
-          imageUrl="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600"
+          imageUrl="https://www.net-a-porter.com/variants/images/1647597318356086/ou/w358_q60.jpg"
         />
         <Product
-          title="Jacket with wol"
+          title="Hurley jean"
           
           newPrice="$65"
           category="women"
-          imageUrl="https://images.pexels.com/photos/2681751/pexels-photo-2681751.jpeg?auto=compress&cs=tinysrgb&w=600"
+          imageUrl="https://is4.revolveassets.com/images/p4/n/d/ROFR-WJ46_V4.jpg"
         />
         <Product
-          title="Compact fashion"
+          title="Sweatshirt"
           
           newPrice="$55.99"
           category="women"
-          imageUrl="https://images.pexels.com/photos/2752045/pexels-photo-2752045.jpeg?auto=compress&cs=tinysrgb&w=600"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F5a%2F63%2F5a63e379c5b780ca1b75a9ffcf759311aa4a1e50.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
         />
         <Product
-          title="Blue jeans"
+          title="Baggy T-shirt"
          newPrice="$50"
           category="Women"
-          imageUrl="https://images.pexels.com/photos/1485031/pexels-photo-1485031.jpeg?auto=compress&cs=tinysrgb&w=600"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fcf%2F86%2Fcf86ad6040e311fb7f3ddf64f6a3dc66851c5780.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
         />
         <Product
-          title="Long sleeve Jac"
+          title="trouser set and top"
         
-          newPrice="$150"
-          category="Women"
-          imageUrl="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600"
+          newPrice="$70"
+          category="Kids"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F28%2F20%2F28205c6836c793c380eb251bb0d189f1f6ac1b02.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
         />
         <Product
-          title="Long sleeve Jac"
-          newPrice="$150"
-          category="Women"
-          imageUrl="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600"
+          title="2 piece cotton set"
+          newPrice="$44.8"
+          category="Kids"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2Fab%2Fb2%2Fabb2315ed7a538fe8193e8f7699182d7de2c9951.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
         />
         <Product
-          title="Long sleeve Jac"
+          title="printed sweatshirt"
          
-          newPrice="$150"
-          category="Women"
-          imageUrl="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600"
+          newPrice="$50"
+          category="Kids"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F04%2F88%2F0488f205dabe7b9589015dfa62e3faf58f3a68f4.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
         />
         <Product
-          title="Long sleeve Jac"
-          newPrice="$150"
-          category="Women"
-          imageUrl="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600"
+          title="Tulle dress"
+          newPrice="$88.99"
+          category="Kids"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fc4%2F75%2Fc4759c8bb3a0b030a2ca8b8ffa7606dedd4f5f43.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D"
         />
         <Product
-          title="Long sleeve Jac"
-          newPrice="$150"
-          category="Women"
-          imageUrl="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600"
+          title="teddy jacket"
+          newPrice="$200"
+          category="Men"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F17%2F82%2F1782d4c4ef0dad7e56435a3b75e35d300dfa75c4.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D"
         />
         <Product
-          title="Long sleeve Jac"
+          title="flannel shirt"
           newPrice="$150"
-          category="Women"
-          imageUrl="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600"
+          category="Men"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F27%2F25%2F27258d7e45094960c65d609ccfe0c02d356ebaef.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D"
         />
         <Product
-          title="Long sleeve Jac"
-          newPrice="$150"
-          category="Women"
-          imageUrl="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600"
+          title="black jean"
+          newPrice="$90"
+          category="Men"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F62%2Fee%2F62ee46d755fa18d6b48018a3b4ddb6710edd6e8e.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]"
         />
         <Product
-          title="Long sleeve Jac"
-          newPrice="$150"
-          category="Women"
-          imageUrl="https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg?auto=compress&cs=tinysrgb&w=600"
+          title="Hoodie"
+          newPrice="$99"
+          category="Men"
+          imageUrl="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F5d%2F8f%2F5d8fd0b89f9f0fe71af936d9ca687b53b3bc410e.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D"
         />
         {/* Add more Product components for other items */}
       </div>
