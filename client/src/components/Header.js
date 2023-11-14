acffb2075cf4c5ae4c794097d45737f939619fa5
Midhunnnnnnnn <midhunsm114@gmail.com>
2023-11-14 18:52:03 +0530

    first8

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,71 +1,69 @@
-import React from 'react';
-import {logo1} from '../assets/index';
+import React from "react";
 import {home} from '../assets/index';
 import {cart} from '../assets/index';
 import {login} from '../assets/index';
-import {shop} from '../assets/index';
 
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { cartImg, logoDark } from "../assets/index";
 
 const Header = () => {
-    const headerStyle = {
-        backgroundColor: 'rgb(5, 59, 80)',
-        width: '100%',
-        height: '70px',
-    };
-
-    const logoStyle = {
-        color: 'rgb(238, 238, 238)', 
-    };
-
-    const inputStyle={
-        color: 'rgb(238, 238, 238)',
-        border: 'none',
-        width: '500px', 
-        borderRadius: '20px', 
-        padding: '8px'
-    };
-
-    const cartStyle = {
-        color: 'rgb(238, 238, 238)', 
-    };
-
-    const loginStyle = {
-        color: 'rgb(238, 238, 238)',
-    };
-    const inputContainerStyle={
-        position: 'relative',
-        display: 'flex',
-        alignItems: 'center',
-    };
-    const iconStyle={
-        position: 'absolute',
-        left: '10px',
-        top: '50%',
-        transform: 'translateY(-50%)',
-        color: 'rgb(150, 150, 150)', // Icon color
-    };
-    return (
-        <div style={headerStyle}>
-            <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
-                <div style={logoStyle}>
-                    <img className='w-35 h-10' src={logo1}alt="logo1"/>
-                </div>
-                <div style={inputContainerStyle}>
-                <i className="material-icons" style={iconStyle}>
-                </i>
-                    <input type="text" placeholder="Search for Products" style={inputStyle}/>
-                </div>
-                <div>
-                    <ul className="flex items-center gap-8">
-                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300" style={cartStyle}><img className='w-6 h-6' src={home}alt="home"/></li>
-                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"style={loginStyle}><img  className='w-6 h-6'src={shop}alt="shop"/></li>
-                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"style={loginStyle}><img  className='w-6 h-6'src={cart}alt="cart"/></li>
-                        <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"style={loginStyle}><img  className='w-6 h-6'src={login}alt="login"/></li>
-                    </ul>
-                </div>
+  const productData = useSelector((state) => state.bazar.productData);
+  const userInfo = useSelector((state) => state.bazar.userInfo);
+  return (
+    <div className="w-full h-20 bg-white font-titleFont border-b-[1px] border-b-gray-800 sticky top-0 z-50">
+      <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
+        <Link to="/">
+          <div>
+            <img className="w-28" src={logoDark} alt="logoDark" />
+          </div>
+        </Link>
+        <div className="flex items-center gap-8">
+          <ul className="flex item-center gap-8">
+            <Link to="/">
+              <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"><img className='w-6 h-6' src={home}alt="home"/>
+                Home
+              </li>
+            </Link>
+            
+            <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"><img className='w-6 h-6' src={cart}alt="cart"/>
+              Shop
+            </li>
+            
+            <li className="text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"><img className='w-6 h-6' src={login}alt="login"/>
+              Blog
+            </li>
+          </ul>
+          <Link to="/cart">
+            <div className="relative">
+              <img className="w-6" src={cartImg} alt="cartImg" />
+              <span className="absolute w-6 top-2 left-0 text-sm flex items-center justify-center font-semibold font-titleFont">
+                {productData.length}
+              </span>
             </div>
+          </Link>
+
+          <Link to="/login">
+            <img
+              className="w-8 h-8 rounded-full"
+              src={
+                userInfo
+                  ? userInfo.image
+                  : "https://images.pexels.com/photos/264547/pexels-photo-264547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              }
+              alt="userLogo"
+            />
+          </Link>
+
+          {userInfo && (
+            <p className="text-base font-titleFont font-semibold underline underline-offset-2">
+              {userInfo.name}
+            </p>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
