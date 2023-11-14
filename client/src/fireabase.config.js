fc5e94af1528a80c65ad9769294569474715fcd3
Midhunnnnnnnn <midhunsm114@gmail.com>
2023-11-14 19:07:43 +0530

    final

--- client/src/fireabase.config.js
+++ client/src/fireabase.config.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
@@ -13,5 +12,4 @@ const firebaseConfig = {
 };
 
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
